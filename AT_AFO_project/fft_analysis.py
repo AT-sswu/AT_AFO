97d17c5ed9ae7846140556ce6d03ef48c65745ea
kseohyuz <seohyun518612@naver.com>
2025-05-09 00:31:45 +0900

    Feat: 공진 주파수 추출 기능 추가

--- AT_AFO_project/fft_analysis.py
+++ AT_AFO_project/fft_analysis.py
@@ -1,9 +1,27 @@
 import numpy as np
+import matplotlib.pyplot as plt
 
-# FFT 분석 함수
-def fft_analysis(data, sample_rate):
+# FFT 분석
+def fft_analysis(data, sample_rate, plot=True):
     n = len(data)
     x = np.fft.fftfreq(n, 1 / sample_rate)
     y = np.fft.fft(data) / n
     mask = x >= 0
-    return x[mask], np.abs(y[mask])
+    freqs = x[mask]
+    amps = np.abs(y[mask])
+
+    # 공진 주파수 (최대 진폭을 가지는 주파수)
+    resonance_freq = freqs[np.argmax(amps)]
+
+    if plot:
+        plt.figure(figsize=(10, 5))
+        plt.plot(freqs, amps)
+        plt.title("FFT Frequency Spectrum")
+        plt.xlabel("Frequency (Hz)")
+        plt.ylabel("Amplitude")
+        plt.grid(True)
+        plt.axvline(resonance_freq, color='r', linestyle='--', label=f'Resonance: {resonance_freq:.2f} Hz')
+        plt.legend()
+        plt.show()
+
+    return freqs, amps, resonance_freq
